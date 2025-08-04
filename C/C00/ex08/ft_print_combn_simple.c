cf9977cbfd8adec5bd260c650fdb8bcd7e86929d
Abderrahman Arid <arid07abderrahman@gmail.com>
2025-08-04 02:16:05 +0100

    1337_w

---
+++ C/C00/ex08/ft_print_combn_simple.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn_simple.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: os-moussao <omoussaoui040@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 17:24:45 by os-moussao        #+#    #+#             */
+/*   Updated: 2021/09/24 17:43:33 by os-moussao       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+ * Another solution for ft_print_combn exercise
+ * I came up with this solution after the pool, and wanted to share it here
+ * it's more efficient and simple to understand
+ * I used backtracking, which - in this case -  acts like multiple nested loops
+ * norminette: OK!
+**/
+#include <unistd.h>
+
+void	show_comma(char stack[], int n)
+{
+    int	  i = 10 - n;
+
+    for (int j = 0; j <= n; j++)
+        if (stack[j] - '0' != i++) {
+            write(1, ", ", 2);
+            return;
+        }
+}
+
+void	backtrack(char stack[], int index, int start, int n)
+{
+    if (index == n) {
+        write(1, buf, n);
+        show_comma(buf, n);
+        return;
+    }
+    while (start <= '9') {
+        stack[index] = start;
+        backtrack(stack, index + 1, ++start, n);
+    }
+}
+
+void	ft_print_combn(int n)
+{
+    char    stack[10];
+
+    backtrack(stack, 0, '0', n);
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# Welcome to Piscine Experience August 2021!
+![HOLY GRAPH](RANDOM/README%20CONTENT/Holy%20Graph.png?raw=true)
+
+In this repository, I shared everything I have done during the 28 days of 1337 piscine.
+
+# Things to consider
+
+## ft_print_combn
+
+> C00/ex08
+
+I added another simple solution [ft_print_combn_simple.c](C/C00/ex08/ft_print_combn_simple.c) using backtracking (don't get scared of the name young novice :joy::heart:)
+
+## ft_ten_queens_puzzle
+
+> C05/ex08
+
+The code I pushed during my piscine, although it was correct, yet I still believe that it's not a good way of solving this problem.  
+  
+Because, when reaching a deadend or a solution, I kept calling the function backwards rather than letting it go to its previous call stacks (if that make sense :slightly_smiling_face:). Thus, I needed to know the number of solutions beforehand if I want the function to quit.  
+  
+Here is another simpler solution to this problem: [the n queens](https://github.com/os-moussao/algorithms-and-data-structures/blob/master/Algorithms/Backtracking/01%20The%20N%20Queens/the_n_queens_puzzle.c)
+
+## bsq
+
+Me and [@awbx](https://github.com/awbx) have worked on the bsq project, all functional tests succeeded, but we got a SEGABORT in the basic test. Therefore, we failed this project with a 50% score.  
+  
+You can take some ideas from what we've done. Or, you may also want to check our friends [@Ceciver](https://github.com/ceciver) and [@XMoulinette](https://github.com/XMoulinette)'s [BSQ](https://github.com/XMoulinette/BSQ_100) with a 100% success score.
+
+# My Scores
+![Scores img.](/RANDOM/README%20CONTENT/Scores.png?raw=true)
+
+# Contact
+[@osmoussao](https://twitter.com/osmoussao)
